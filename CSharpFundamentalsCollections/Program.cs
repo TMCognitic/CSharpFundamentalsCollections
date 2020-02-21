35b36f0522440f151bc487c6ad6b0a9fe799f844
Thierry Morre <thierry.morre@cognitic.be>
2020-02-21 12:04:52 +0100

    Solution Exercice 1

--- CSharpFundamentalsCollections/Program.cs
+++ CSharpFundamentalsCollections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace CSharpFundamentalsCollections
 {
@@ -6,7 +7,44 @@ namespace CSharpFundamentalsCollections
     {
         static void Main(string[] args)
         {
-            
+            #region Exercice 1
+            {
+                int maxValue;
+                do
+                {
+                    Console.Write("Jusqu'a quelle valeur voulez-vous trouver les nombres premiers ? : ");
+                }
+                while (!int.TryParse(Console.ReadLine(), out maxValue) && maxValue > 0);
+
+                ArrayList primes = new ArrayList();
+
+                int nombre = 2;
+
+                while (nombre < maxValue)
+                {
+                    bool isPrime = true;
+                    int racine = (int)Math.Sqrt(nombre);
+
+                    for (int index = 0; racine > 1 && (int)primes[index] <= racine && isPrime; index++)
+                    {
+                        if (nombre % (int)primes[index] == 0)
+                            isPrime = false;
+                    }
+
+                    if (isPrime)
+                    {
+                        primes.Add(nombre);
+                    }
+
+                    nombre++;
+                }
+
+                foreach (int prime in primes)
+                {
+                    Console.WriteLine(prime);
+                }
+            }
+            #endregion
         }
     }
 }
